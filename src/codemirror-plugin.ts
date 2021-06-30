7d809dbe0a2ab362e4c320afd2e99651493aa563
John Freeman <jfreeman08@gmail.com>
2021-06-30 18:37:41 -0500

    Add newTodo and a README

--- src/codemirror-plugin.ts
+++ src/codemirror-plugin.ts
@@ -3,12 +3,14 @@ import { ContentScriptContext } from '@thejohnfreeman/joplin-plugin/types'
 
 function plugin(CodeMirror) {
   const Vim = CodeMirror.Vim
-  Vim.defineAction('newNote', () => joplin.commands.execute('newNote'))
   Vim.defineAction('gotoAnything', () =>
     joplin.commands.execute('gotoAnything')
   )
-  Vim.mapCommand('<C-n>', 'action', 'newNote')
+  Vim.defineAction('newNote', () => joplin.commands.execute('newNote'))
+  Vim.defineAction('newTodo', () => joplin.commands.execute('newTodo'))
   Vim.mapCommand('<C-p>', 'action', 'gotoAnything')
+  Vim.mapCommand('<C-n>', 'action', 'newNote')
+  Vim.mapCommand('<C-t>', 'action', 'newTodo')
 }
 
 function load(context: ContentScriptContext) {
